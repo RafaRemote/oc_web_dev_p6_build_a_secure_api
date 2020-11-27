4db95eaeed9ff1c73f6586bdaaab2eb792371bec
raphael <raphael.49410@gmail.com>
2020-11-27 18:05:51 +0100

    modification du readme.md: pas besoin de demander la création d'un dossier images vide

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,16 @@
                         require('dotenv').config()          // load environment variables from the .env file into process.env
 const express =         require('express');                 // import express: node.js web framework
 const bodyParser =      require('body-parser');             // import body-parser: parse incoming request bodies in a middleware before handlers, available under the req.body property.
-const mongoose =        require('mongoose');                // import mongoose: a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports both promises and callbacks.
+const mongoose =        require('mongoose');                // import mongoose: ODM, a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports both promises and callbacks.
 const helmet =          require('helmet');                  // import helmet: help to secure Express apps by setting various HTTP headers.
 const path =            require('path');                    // import path: provides a way of working with directories and file paths. (line 44)
 const cors =            require('cors');                    // import cors: manage cross-origin resource sharing
 const rateLimit =       require('express-rate-limit');      // comme son nom l'indique: on va fixer un taux limite pour les requêtes.
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+const limiter = rateLimit({         
+  windowMs: 15 * 60 * 1000,       // = 15 minutes
   max: 100
 })
-
-// import the routes for user and sauces from directory "routes"
-const saucesRoutes =    require('./routes/sauces');          
-const userRoutes =      require('./routes/user');
-
 
 // Use express
 const app = express();
@@ -57,6 +52,11 @@ app.use(bodyParser.json());
 
 // Images management
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+// import the routes for user and sauces from directory "routes"
+const saucesRoutes =    require('./routes/sauces');          
+const userRoutes =      require('./routes/user');
 
 // API routes 
 app.use('/api/sauces', saucesRoutes);
