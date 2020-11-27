4ced525e13ec95888bfc51848d887c48d2242556
raphael <raphael.49410@gmail.com>
2020-11-27 22:34:32 +0100

    changement dans le dossier images

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -70,47 +70,46 @@ exports.rateOneSauce = (req, res, _) => {
   switch (req.body.like) {
     case 0:                                                   //cas: req.body.like = 0
       Sauce.findOne({ _id: req.params.id })
-      .then((sauce) => {
-        if (sauce.usersLiked.find(user => user === req.body.userId) ){  // on cherche si l'utilisateur est déjà dans le tableau usersLiked
-          Sauce.updateOne({ _id: req.params.id }, {         // si oui, on va mettre à jour la sauce avec le _id présent dans la requête
-            $inc: { likes: -1 },                            // on décrémente la valeur des likes de 1 (soit -1)
-            $pull: { usersLiked: req.body.userId }          // on retire l'utilisateur du tableau.
+        .then((sauce) => {
+          if (sauce.usersLiked.find( user => user === req.body.userId)) {  // on cherche si l'utilisateur est déjà dans le tableau usersLiked
+            Sauce.updateOne({ _id: req.params.id }, {         // si oui, on va mettre à jour la sauce avec le _id présent dans la requête
+              $inc: { likes: -1 },                            // on décrémente la valeur des likes de 1 (soit -1)
+              $pull: { usersLiked: req.body.userId }          // on retire l'utilisateur du tableau.
             })
               .then(() => { res.status(201).json({ message: "vote enregistré."}); }) //code 201: created
               .catch((error) => { res.status(400).json({error}); });
+
           } 
-        if (sauce.usersDisLiked.find(user => user === req.body.userId)) {  //mêmes principes que précédemment avec le tableau usersDisliked
-          Sauce.updateOne({ _id: req.params.id }, {
-            $inc: { dislikes: -1 },
-            $pull: { usersDisliked: req.body.userId }
+          if (sauce.usersDisliked.find(user => user === req.body.userId)) {  //mêmes principes que précédemment avec le tableau usersDisliked
+            Sauce.updateOne({ _id: req.params.id }, {
+              $inc: { dislikes: -1 },
+              $pull: { usersDisliked: req.body.userId }
             })
-          .then(() => { res.status(201).json({ message: "vote enregistré." }); })
-          .catch((error) => { res.status(400).json({error}); });
+              .then(() => { res.status(201).json({ message: "vote enregistré." }); })
+              .catch((error) => { res.status(400).json({error}); });
           }
-       })
-      .catch((error) => { res.status(404).json({message: error}); });
+        })
+        .catch((error) => { res.status(404).json({error}); });
       break;
-  
+    
     case 1:                                                 //cas: req.body.like = 1
       Sauce.updateOne({ _id: req.params.id }, {             // on recherche la sauce avec le _id présent dans la requête
         $inc: { likes: 1 },                                 // incrémentaton de la valeur de likes par 1.
         $push: { usersLiked: req.body.userId }              // on ajoute l'utilisateur dans le array usersLiked.
-        })
-      .then(() => { res.status(201).json({ message: "vote enregistré." }); }) //code 201: created
-      .catch((error) => { res.status(400).json({ error }); }); //code 400: bad request
+      })
+        .then(() => { res.status(201).json({ message: "vote enregistré." }); }) //code 201: created
+        .catch((error) => { res.status(400).json({ error }); }); //code 400: bad request
       break;
-  
+    
     case -1:                                                  //cas: req.body.like = 1
       Sauce.updateOne({ _id: req.params.id }, {               // on recherche la sauce avec le _id présent dans la requête
         $inc: { dislikes: 1 },                                // on décremente de 1 la valeur de dislikes.
         $push: { usersDisliked: req.body.userId }             // on rajoute l'utilisateur à l'array usersDiliked.
-        })
-      .then(() => { res.status(201).json({ message: "vote enregistré." }); }) // code 201: created
-      .catch((error) => { res.status(400).json({ error }); }); // code 400: bad request
+      })
+        .then(() => { res.status(201).json({ message: "vote enregistré." }); }) // code 201: created
+        .catch((error) => { res.status(400).json({ error }); }); // code 400: bad request
       break;
-      
     default:
-    console.error(error);
-    }
-
-}
+      console.error("bad request");
+  }
+};
